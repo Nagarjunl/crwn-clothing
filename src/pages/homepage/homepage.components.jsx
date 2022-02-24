e75b095e8130d8eaf58ccf2d7604e68c18c35b89
Nagarjun <naga.career.at@gmail.com>
2022-02-24 19:52:28 +0530

    Homepage commit and menu styles finished

---
+++ src/pages/homepage/homepage.components.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Directive from '../../components/directive/directive.component';
+import "./homepage.styles.scss";
+
+const HomePage = () => (
+    <div className="homepage">
+        <Directive/>
+    </div>
+);
+
+export default HomePage;
